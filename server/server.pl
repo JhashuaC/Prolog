b802986c13b4fa57ed47da89340ec1ef50918250
Jhashua10 <146078820+JhashuaC@users.noreply.github.com>
2025-11-09 10:44:56 -0600

    Update server.pl

--- server/server.pl
+++ server/server.pl
@@ -2,10 +2,8 @@
 :- set_prolog_flag(encoding, utf8).
 
 :- use_module(library(http/thread_httpd)).
-:- use_module(library(http/http_server)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_json)).
-:- use_module(library(http/html_write)).
 :- use_module(library(http/http_cors)).
 
 :- use_module(server(routes/api_diagnose)).
@@ -16,56 +14,58 @@
 
 :- set_setting(http:cors, [*]).
 
-:- http_handler(root(.),                  ui_page,                           []).
-:- http_handler(root('app.js'),           serve_app_js,                      []).
-:- http_handler(root(api/ping),           api_ping,                          [method(get)]).
-:- http_handler(root(api/symptoms),       h_api_symptoms,                    [method(get)]).
-:- http_handler(root(api/diagnose),       h_api_diagnose,                    [method(post)]).
-:- http_handler(root(api/sintomas_de),              h_api_sintomas_de,              [method(post)]).
-:- http_handler(root(api/enfermedades_por_sintoma), h_api_enfermedades_por_sintoma, [method(post)]).
-:- http_handler(root(api/categoria_enfermedad),     h_api_categoria_enfermedad,     [method(post)]).
-:- http_handler(root(api/enfermedades_posibles),    h_api_enfermedades_posibles,    [method(post)]).
-:- http_handler(root(api), options_cors, [method(options), prefix]).
+:- http_handler(root(.), ui_page, []).
+:- http_handler(root('app.js'), serve_app_js, []).
+:- http_handler(root(api/ping), api_ping, [method(get)]).
+:- http_handler(root(api/symptoms), api_symptoms_handler, [method(get)]).
+:- http_handler(root(api/diagnose), api_diagnose_handler, [method(post)]).
+:- http_handler(root(api/sintomas_de), api_sintomas_de_handler, [method(post)]).
+:- http_handler(root(api/enfermedades_por_sintoma), api_enfermedades_por_sintoma_handler, [method(post)]).
+:- http_handler(root(api/categoria_enfermedad), api_categoria_enfermedad_handler, [method(post)]).
+:- http_handler(root(api/enfermedades_posibles), api_enfermedades_posibles_handler, [method(post)]).
+:- http_handler(root(api), cors_options_handler, [method(options), prefix]).
 
-allow_cors(Request) :-
-	cors_enable(Request,
-		[
-			methods([get, post, options]),
-			origins(['*'])]).
+add_cors_headers :-
+	format('Access-Control-Allow-Origin: *~n'),
+	format('Access-Control-Allow-Methods: GET, POST, OPTIONS~n'),
+	format('Access-Control-Allow-Headers: Content-Type, Authorization~n').
+
+cors_options_handler(_Request) :-
+	add_cors_headers,
+	format('Status: 204~n~n').
 
 api_ping(Request) :-
-	allow_cors(Request),
+	add_cors_headers,
 	reply_json_dict(_{ok : true, ts : now}).
 
-h_api_symptoms(Request) :-
-	allow_cors(Request),
+api_symptoms_handler(Request) :-
+	add_cors_headers,
 	api_symptoms(Request).
 
-h_api_diagnose(Request) :-
-	allow_cors(Request),
+api_diagnose_handler(Request) :-
+	add_cors_headers,
 	api_diagnose(Request).
 
-h_api_sintomas_de(Request) :-
-	allow_cors(Request),
+api_sintomas_de_handler(Request) :-
+	add_cors_headers,
 	api_queries : api_sintomas_de(Request).
 
-h_api_enfermedades_por_sintoma(Request) :-
-	allow_cors(Request),
+api_enfermedades_por_sintoma_handler(Request) :-
+	add_cors_headers,
 	api_queries : api_enfermedades_por_sintoma(Request).
 
-h_api_categoria_enfermedad(Request) :-
-	allow_cors(Request),
+api_categoria_enfermedad_handler(Request) :-
+	add_cors_headers,
 	api_queries : api_categoria_enfermedad(Request).
 
-h_api_enfermedades_posibles(Request) :-
-	allow_cors(Request),
+api_enfermedades_posibles_handler(Request) :-
+	add_cors_headers,
 	api_queries : api_enfermedades_posibles(Request).
 
-options_cors(Request) :-
-	allow_cors(Request),
-	reply_json_dict(_{
-			},
-		[status(204)]).
+serve_app_js(_Request) :-
+	http_reply_file('./static/app.js',
+		[unsafe(true), mime_type('application/javascript')],
+		[]).
 
 server(Port) :-
 	ignore(stop_all),
@@ -90,9 +90,8 @@ stop_all :-
 			(log(red, 'Cerrando servidor en puerto ~w...~n', [P]),
 				catch(http_stop_server(P,
 						[force(true)]),
-					E,
-					log(red, ' Error al cerrar servidor (~w): ~w~n', [P, E])))),
-		log(green, 'Servidores HTTP detenidos correctamente.~n', [])),
+					_,
+					true)))),
 	sleep(0.2),
 	repeat,
 	findall(Id - Name,
@@ -109,14 +108,9 @@ stop_all :-
 						true),
 					catch(thread_join(Id, _),
 						_,
-						true) ->
+						true)) ->
 	log(yellow, ' Hilo residual terminado: ~w~n', [Name]);
-	log(gray, 'Esperando cierre natural: ~w~n', [Name])))),
+	log(gray, 'Esperando cierre natural: ~w~n', [Name]))),
 		sleep(0.1),
 		fail),
 	log(green, ' Todos los hilos HTTP terminados correctamente.~n', []).
-
-serve_app_js(_Request) :-
-	http_reply_file('./static/app.js',
-		[unsafe(true), mime_type('application/javascript')],
-		[]).
