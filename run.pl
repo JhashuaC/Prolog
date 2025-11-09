418d26ed1d49250eb6b8c977570b3789cbe0dd23
Jhashua10 <146078820+JhashuaC@users.noreply.github.com>
2025-11-08 20:38:08 -0600

    Update run.pl

--- run.pl
+++ run.pl
@@ -5,18 +5,13 @@
 :- use_module('server/server.pl', [server/1, stop_all/0]).
 
 main :-
-	% Obtener el puerto asignado por Railway
-(getenv('PORT', PortAtom) ->
+	(getenv('PORT', PortAtom) ->
 	atom_number(PortAtom, Port);
-	Port = 8080% Fallback local
-	),
-	% Detener servidores activos previos
+	Port = 8080),
 	catch(stop_all, _, true),
-	% Iniciar servidor
 	format('🚀 Iniciando servidor en puerto ~w~n', [Port]),
 	catch(server(Port),
 		E,
 		(print_message(error, E),
 			halt(1))),
-	% Mantener proceso vivo (Railway necesita esto)
 	thread_get_message(quit).
